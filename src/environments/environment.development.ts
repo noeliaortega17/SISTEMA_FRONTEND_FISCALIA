cb040d16df105ebff05ba03f66c923752aa0ad08
ALVARO <alvaroortega801@gmail.com>
2023-12-05 22:01:27 -0400

    commit de Alvaro Ortega

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     server_url:'http://localhost:8085/fiscalia/v1/',
     server_security_url:'http://localhost:8086/security/v1/',
-    formatDate: 'DD/MM/YYYY'
+    formatDate: 'DD/MM/YYYY' //formato de dia mes y año
 };
